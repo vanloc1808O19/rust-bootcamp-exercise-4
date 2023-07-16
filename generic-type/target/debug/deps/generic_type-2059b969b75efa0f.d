4ef68e530dc474e353d2459d26f031760269cde2
Loc Nguyen <loc.nguyen@saritasa.com>
2023-07-16 12:00:07 +0700

    Implement generic type

---
+++ generic-type/target/debug/deps/generic_type-2059b969b75efa0f.d
@@ -0,0 +1,5 @@
+/Users/vanloc1808/GitHub/rust-bootcamp-exercise-4/generic-type/target/debug/deps/generic_type-2059b969b75efa0f: src/lib.rs
+
+/Users/vanloc1808/GitHub/rust-bootcamp-exercise-4/generic-type/target/debug/deps/generic_type-2059b969b75efa0f.d: src/lib.rs
+
+src/lib.rs:
---
+++ generic-type/target/debug/deps/generic_type-a104335e91d4e17c.d
@@ -0,0 +1,7 @@
+/Users/vanloc1808/GitHub/rust-bootcamp-exercise-4/generic-type/target/debug/deps/generic_type-a104335e91d4e17c.rmeta: src/lib.rs
+
+/Users/vanloc1808/GitHub/rust-bootcamp-exercise-4/generic-type/target/debug/deps/libgeneric_type-a104335e91d4e17c.rlib: src/lib.rs
+
+/Users/vanloc1808/GitHub/rust-bootcamp-exercise-4/generic-type/target/debug/deps/generic_type-a104335e91d4e17c.d: src/lib.rs
+
+src/lib.rs:
