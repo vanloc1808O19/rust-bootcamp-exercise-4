41dcf89acb915d3392daa34a8d90c30072deef20
Loc Nguyen <loc.nguyen@saritasa.com>
2023-07-16 12:09:23 +0700

    Implemenet traits

---
+++ traits/target/debug/deps/traits-852ad1be9ab0b7a7.d
@@ -0,0 +1,5 @@
+/Users/vanloc1808/GitHub/rust-bootcamp-exercise-4/traits/target/debug/deps/traits-852ad1be9ab0b7a7: src/lib.rs
+
+/Users/vanloc1808/GitHub/rust-bootcamp-exercise-4/traits/target/debug/deps/traits-852ad1be9ab0b7a7.d: src/lib.rs
+
+src/lib.rs:
---
+++ traits/target/debug/deps/traits-b197907af8583e63.d
@@ -0,0 +1,7 @@
+/Users/vanloc1808/GitHub/rust-bootcamp-exercise-4/traits/target/debug/deps/traits-b197907af8583e63.rmeta: src/lib.rs
+
+/Users/vanloc1808/GitHub/rust-bootcamp-exercise-4/traits/target/debug/deps/libtraits-b197907af8583e63.rlib: src/lib.rs
+
+/Users/vanloc1808/GitHub/rust-bootcamp-exercise-4/traits/target/debug/deps/traits-b197907af8583e63.d: src/lib.rs
+
+src/lib.rs:
